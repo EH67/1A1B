fd25b6bf6ad37f20cb1f2db5b881b337e5501ba3
EH67 <68453558+EH67@users.noreply.github.com>
2021-06-16 20:53:31 -0700

    Update 1A1B only me.py

--- 1A1B only me.py
+++ 1A1B only me.py
@@ -4,7 +4,7 @@ num = random.sample('0123456789', 4)
 while num[0] == '0': #this loop ensures the first number isn't 0
     num = random.sample('0123456789', 4)
 
-outfile = open('1A1B_data', 'a')
+
 
 answer = ''.join(num)
 guess = int(input('enter four digits: '))
@@ -12,8 +12,7 @@ guesslist = [*(str(guess))]
 j = 1
 A = 0
 B = 0
-outfile.write('the correct answer is {}\n'.format(''.join(num)))
-outfile.write('guess {}: {}'.format(j, guess))
+
 while int(answer) != int(guess):
     hello = set(guesslist)
     while len(hello) != 4: #this tiny while loop checks if user enters duplicates
@@ -27,18 +26,16 @@ while int(answer) != int(guess):
             B += 1
         else:
             pass
-    outfile.write('\t{}A{}B\n'.format(A,B))
+    
     print('You guessed {}. {}A{}B'.format(guess, A, B))
     if int(answer) == int(guess):
         break
     guess = int(input('enter four digits: '))
     guesslist = [*str(guess)]
     j += 1
-    outfile.write('guess {}: {}'.format(j, guess))
+   
     A = 0
     B = 0
 
 print('Congrats! You guessed the right number! This took you {} tries!'.format(j))
 
-outfile.write('\n\ttook a total of {} tries\n\n\n'.format(j))
-outfile.close()
